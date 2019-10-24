ab37227b0f47a2c2862b51dee83bd42edad36e34
OczaneM <oczane.rivera@gmail.com>
2019-10-24 09:45:10 -0400

    Fix slide and rotate animations

--- source.js
+++ source.js
@@ -4,19 +4,13 @@ const getElement = element => document.querySelector(element);
 const bars = getElement(".center");
 const firstBar = getElement(".firstbar");
 const secondBar = getElement(".secondbar");
-const thirdBar = getElement(".thirdbar")
+const thirdBar = getElement(".thirdbar");
 
 /* Methods */
 const handleBarsClick = event => {
-	secondBar.classList.toggle("shrink");
-	firstBar.classList.toggle("slide_animation");
-	firstBar.classList.toggle("rotate_animation");
-	firstBar.classList.toggle("firstbar_finish");
-	// thirdBar.classList.toggle("slideup");
-	// firstBar.classList.toggle("rotateright");
-	// thirdBar.classList.toggle("rotateleft");
-}
+  secondBar.classList.toggle("shrink");
+  // firstBar.classList.toggle("slide_animation");
+  firstBar.classList.toggle("rotate_animation");
+};
 
 bars.addEventListener("click", handleBarsClick);
-
-
